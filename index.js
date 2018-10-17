0ff83d2f3bcdda232198c5f6d10f4e96ecec1798
Azrty <38593367+Azrty@users.noreply.github.com>
2018-10-17 17:34:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ bot.on("message", message => {
             mess.react('✅')
             mess.react('❌')
             const track = mess.createReactionCollector((r, u, nsfwmsg) => u.id === message.author.id)
-            track.on('collect', (r, nsfwmsg) => {
+            track.on('collect', r, nsfwmsg => {
                 if (r.emoji.name === "✅") { 
                 message.channel.bulkDelete(1)
                 message.delete()
