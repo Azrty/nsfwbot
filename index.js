75895ad680fd17fb972d6a4bb6e79125a96b401f
Azrty <38593367+Azrty@users.noreply.github.com>
2018-10-17 17:23:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ bot.login(process.env.TOKEN);
 bot.on("message", message => {
 
     if(message.content === prefix + "nsfw") {
-        var nsfwmsg = message.channel.send("Cet accès peut choquer ou toucher la sensibilité des plus jeunes \nVous êtes responsable de ce que vous allez voir.\nÊtes vous sûr d'avoir accès au NSFW ?").then(mess => {
+        message.channel.send("Cet accès peut choquer ou toucher la sensibilité des plus jeunes \nVous êtes responsable de ce que vous allez voir.\nÊtes vous sûr d'avoir accès au NSFW ?").then(mess => {
             mess.react('✅')
             mess.react('❌')
             const track = mess.createReactionCollector((r, u) => u.id === message.author.id)
@@ -18,8 +18,8 @@ bot.on("message", message => {
                     message.member.addRole(role)
                     message.reply("Vous avez bien reçu l'accès au NSFW")}
                 if (r.emoji.name === "❌") {
-                    //message.channel.bulkDelete(1)
-                    nsfwmsg.delete()
+                    message.channel.bulkDelete(1)
+                    //nsfwmsg.delete()
                     message.channel.send("Demande d'accès au NSFW annulée !")}
 
 
